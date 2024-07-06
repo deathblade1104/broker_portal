281792eff9355df76c6712418e41367656db0573
Shahbaz Hasan Raja <shahbaz.raja@wework.co.in>
2024-07-06 07:00:26 +0530

    User APIs corrected

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,5 +1,11 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import {
+  ApiCustomResponseArray,
+  ApiCustomResponseObject,
+} from '../../common/decorators/api-response.decorator';
+import { CustomResponseBody } from '../../common/providers/customResponse';
+import { User } from './entities/user.entity';
 import { UsersService } from './users.service';
 
 @ApiTags('users')
@@ -13,13 +19,21 @@ export class UsersController {
   // }
 
   @Get()
-  findAll() {
-    return this.usersService.findAll();
+  @ApiCustomResponseArray(User)
+  async findAll() {
+    return new CustomResponseBody(
+      'Fetched all users successfully',
+      await this.usersService.findAll(),
+    );
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.usersService.findById(+id);
+  @ApiCustomResponseObject(User)
+  async findOne(@Param('id') id: string) {
+    return new CustomResponseBody(
+      `Fetched user with Id - ${id} successfully`,
+      await this.usersService.findById(+id),
+    );
   }
 
   // @Patch(':id')
